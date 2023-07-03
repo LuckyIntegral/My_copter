b10bb94ea373e5c42fa5e0e5ff4263424ffbc856
Vitalik <frantsv2004@gmail.com>
2023-07-03 18:23:55 +0200

    First commit

---
+++ copter_server/src/main/java/my/copter/CopterServerApplication.java
@@ -0,0 +1,11 @@
+package my.copter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopterServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CopterServerApplication.class, args);
+    }
+}
