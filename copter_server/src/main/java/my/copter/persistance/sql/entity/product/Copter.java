5c59a43b78607b9e9ee7334eaae65526f9897a46
Vitalik <frantsv2004@gmail.com>
2023-07-04 23:30:25 +0200

    add entity package

---
+++ copter_server/src/main/java/my/copter/persistance/sql/entity/product/Copter.java
@@ -0,0 +1,66 @@
+package my.copter.persistance.sql.entity.product;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import my.copter.persistance.sql.entity.BaseEntity;
+import my.copter.persistance.sql.type.BrandType;
+import my.copter.persistance.sql.type.CategoryType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "copters")
+public class Copter extends BaseEntity {
+
+    @Column(name = "brand", nullable = false, updatable = false)
+    @Enumerated(EnumType.STRING)
+    private BrandType brand;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "camera_resolution", nullable = false)
+    private String cameraResolution;
+
+    @Column(name = "fpv_camera")
+    private Boolean fpvCamera;
+
+    @Column(name = "category_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CategoryType categoryType;
+
+    @Column(name = "battery", nullable = false)
+    private String battery;
+
+    @Column(name = "fly_time", nullable = false)
+    private String flyTime;
+
+    @Column(name = "price", nullable = false)
+    private Long price;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @ManyToMany
+    @JoinTable(
+            name = "copter_image",
+            joinColumns = @JoinColumn(name = "copter_id"),
+            inverseJoinColumns = @JoinColumn(name = "copter_image_id")
+    )
+    private Set<CopterImage> copterImages;
+
+    public Copter() {
+        super();
+        this.fpvCamera = false;
+        this.copterImages = new HashSet<>();
+    }
+}
---
+++ copter_server/src/main/java/my/copter/persistance/sql/entity/product/CopterImage.java
@@ -0,0 +1,33 @@
+package my.copter.persistance.sql.entity.product;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import my.copter.persistance.sql.entity.BaseEntity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "copter_images")
+public class CopterImage extends BaseEntity {
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @Column(name = "mainImage", nullable = false)
+    private Boolean mainImage;
+
+    @ManyToMany(mappedBy = "copterImages")
+    private Set<Copter> copters;
+
+    public CopterImage() {
+        super();
+        this.mainImage = false;
+        this.copters = new HashSet<>();
+    }
+}
