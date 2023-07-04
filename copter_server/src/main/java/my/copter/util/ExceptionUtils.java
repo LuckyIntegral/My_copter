7722aa84708d3bb1f7231c51ae5f695cba1d8873
Vitalik <frantsv2004@gmail.com>
2023-07-04 23:33:38 +0200

    add util

---
+++ copter_server/src/main/java/my/copter/util/ExceptionUtils.java
@@ -0,0 +1,8 @@
+package my.copter.util;
+
+public final class ExceptionUtils {
+    public static final String ENTITY_NOT_FOUND = "Entity doesn't exist";
+    public static final String EMPTY_FIELD_EXCEPTION = "Empty field exception";
+
+    private ExceptionUtils() {}
+}
