b5cc3453dc6fc88f289ddff2d9b866a9c6fd5cd1
Vitalik <frantsv2004@gmail.com>
2023-07-05 21:34:46 +0200

    add security context

--- copter_server/src/main/java/my/copter/config/security/service/LogoutService.java
+++ copter_server/src/main/java/my/copter/config/security/service/LogoutService.java
@@ -9,8 +9,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
 
