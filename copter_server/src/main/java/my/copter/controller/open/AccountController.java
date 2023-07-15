a0b701f3fede9df42a022cfb474a53015655b8db
Vitalik <frantsv2004@gmail.com>
2023-07-16 01:14:56 +0200

    add role controller

--- copter_server/src/main/java/my/copter/controller/open/AccountController.java
+++ copter_server/src/main/java/my/copter/controller/open/AccountController.java
@@ -29,6 +29,7 @@ public class AccountController {
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
+
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
